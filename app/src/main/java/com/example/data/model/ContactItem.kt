a84bae487b249986e1e6bc39adce93ad8358d363
StarApp0003 <marcobenitez209@gmail.com>
2026-09-04 13:39:14 +0200

    Delete app directory

--- app/src/main/java/com/example/data/model/ContactItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.data.model
-
-data class ContactItem(
-    val id: String,
-    val name: String,
-    val number: String,
-    val photoUri: String? = null,
-    val initials: String = name.take(2).uppercase()
-)
--- app/src/main/java/com/example/data/model/MessageStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.example.data.model
-
-enum class MessageStatus {
-    PENDING,
-    SENDING,
-    SENT,
-    DELIVERED,
-    FAILED
-}
